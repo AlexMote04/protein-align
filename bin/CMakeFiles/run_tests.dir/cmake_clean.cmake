b48e63d45916ebd4dacac710bab2c211469210a4
Alex Mote <alex.lewis.mote@gmail.com>
2026-01-27 16:33:30 +0000

    Integrate Catch2 Testing Framework

---
+++ bin/CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_tests.dir/src/parse.cpp.o"
+  "CMakeFiles/run_tests.dir/src/parse.cpp.o.d"
+  "CMakeFiles/run_tests.dir/test/test_main.cpp.o"
+  "CMakeFiles/run_tests.dir/test/test_main.cpp.o.d"
+  "CMakeFiles/run_tests.dir/test/test_parser.cpp.o"
+  "CMakeFiles/run_tests.dir/test/test_parser.cpp.o.d"
+  "run_tests"
+  "run_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
