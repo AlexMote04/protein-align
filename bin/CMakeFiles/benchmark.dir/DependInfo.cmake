c36329dc7c19d5fd531fa43b0d12e9e599cea54b
Alex Mote <alex.lewis.mote@gmail.com>
2026-02-20 21:38:05 +0000

    add benchmarking. Note a mismatch between cpu and gpu results with db size of 10000

---
+++ bin/CMakeFiles/benchmark.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alex/Code/protein-align/src/sw_gpu.cu" "CMakeFiles/benchmark.dir/src/sw_gpu.cu.o" "gcc" "CMakeFiles/benchmark.dir/src/sw_gpu.cu.o.d"
+  "/home/alex/Code/protein-align/src/benchmark.cpp" "CMakeFiles/benchmark.dir/src/benchmark.cpp.o" "gcc" "CMakeFiles/benchmark.dir/src/benchmark.cpp.o.d"
+  "/home/alex/Code/protein-align/src/parse.cpp" "CMakeFiles/benchmark.dir/src/parse.cpp.o" "gcc" "CMakeFiles/benchmark.dir/src/parse.cpp.o.d"
+  "/home/alex/Code/protein-align/src/sw_cpu.cpp" "CMakeFiles/benchmark.dir/src/sw_cpu.cpp.o" "gcc" "CMakeFiles/benchmark.dir/src/sw_cpu.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
